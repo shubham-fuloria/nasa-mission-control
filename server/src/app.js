474d733ed774e6dc0137d84ecc6a9ec27aa4f461
shubhamfuloria <sfuloria10200@gmail.com>
2022-10-05 18:14:44 +0530

    created build

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const planetsRouter = require('./routes/plantes.router')
 
@@ -6,6 +7,12 @@ const app = express()
 
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter)
+app.use('/', (req, res) => {
+  // console.log('request to root')
+  // res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
